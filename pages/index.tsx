bcf476beb2ba0257c443b2add7fed28769594af7
Shane Sweeney <shanep.sween@gmail.com>
2021-01-26 19:23:32 -0800

    Refactor to Typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import ProTip from '../src/ProTip';
-import Link from '../src/Link';
 import Copyright from '../src/Copyright';
+import { Button, Link } from '@material-ui/core';
 
 export default function Index() {
   return (
@@ -13,9 +13,11 @@ export default function Index() {
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js example
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
+<Link href="/about">
+        <Button variant="contained" color="primary">
+                    Go to the About page
+        </Button>
+</Link>
         <ProTip />
         <Copyright />
       </Box>
