b6e9a30b86056201ba4763eb201ed627d290c4b6
Shane Sweeney <shanep.sween@gmail.com>
2021-01-26 19:32:20 -0800

    prettier format

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
-import { Button, Link } from '@material-ui/core';
+import React from 'react'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+import ProTip from '../src/ProTip'
+import Copyright from '../src/Copyright'
+import { Button, Link } from '@material-ui/core'
 
 export default function Index() {
   return (
@@ -13,14 +13,14 @@ export default function Index() {
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js example
         </Typography>
-<Link href="/about">
-        <Button variant="contained" color="primary">
-                    Go to the About page
-        </Button>
-</Link>
+        <Link href="/about">
+          <Button variant="contained" color="primary">
+            Go to the About page
+          </Button>
+        </Link>
         <ProTip />
         <Copyright />
       </Box>
     </Container>
-  );
+  )
 }
