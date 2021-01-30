0b372a8dd3dc3e3e2e33c11adb6719d9d76cd9a3
Shane Sweeney <shanep.sween@gmail.com>
2021-01-29 18:18:47 -0800

    File structure change

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -2,9 +2,9 @@ import React, { Fragment } from 'react'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
-import ProTip from '../src/Components/ProTip'
-import Link from '../src/Link'
-import Copyright from '../src/Components/Copyright'
+import ProTip from '../../src/Components/ProTip'
+import Link from '../../src/Link'
+import Copyright from '../../src/Components/Copyright'
 import {
   Avatar,
   Button,
